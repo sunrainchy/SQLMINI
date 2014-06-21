2b29b1ec40b9181c499db9b4acf11a158bcf3f09
chy <1498752974@qq.com>
2014-06-21 17:17:39 +0800

    Make the code more clean

--- data_produce.c
+++ data_produce.c
@@ -7,40 +7,40 @@ char str[1024];
 char name[1024];
 int main()
 {
-srand((unsigned)time(NULL));
-int i,j,k,l,m,n;
-printf("请输入文件名\n");
-scanf("%s",name);
-printf("请输入要生成元组条数\n");
-scanf("%d",&k);
-FILE *fd=fopen(name,"a+");
-if(fd==NULL)
-{
-printf("文件打开错误\n");
-return 0;
-}
-while(k--)
-{
-m=0;
-for(i=0;i<10;i++)
-str[m++]=ch[rand()%26];
-str[m++]=' ';
-for(i=0;i<13;i++)
-str[m++]=num[rand()%10];
-str[m++]=' ';
-for(i=0;i<2;i++)
-str[m++]=num[rand()%10];
-str[m++]=' ';
-for(i=0;i<2;i++)
-str[m++]=num[rand()%10];
-str[m++]=' ';
-for(i=0;i<10;i++)
-str[m++]=num[rand()%10];
-str[m]=0;
-printf("%d ",k);
-fprintf(fd,"%s\n",str);
-}
-fclose(fd);
-return 0;
+	srand((unsigned)time(NULL));
+	int i,j,k,l,m,n;
+	printf("请输入文件名\n");
+	scanf("%s",name);
+	printf("请输入要生成元组条数\n");
+	scanf("%d",&k);
+	FILE *fd=fopen(name,"a+");
+	if(fd==NULL)
+	{
+		printf("文件打开错误\n");
+		return 0;
+	}
+	while(k--)
+	{
+		m=0;
+		for(i=0;i<10;i++)
+			str[m++]=ch[rand()%26];
+		str[m++]=' ';
+		for(i=0;i<13;i++)
+			str[m++]=num[rand()%10];
+		str[m++]=' ';
+		for(i=0;i<2;i++)
+			str[m++]=num[rand()%10];
+		str[m++]=' ';
+		for(i=0;i<2;i++)
+			str[m++]=num[rand()%10];
+		str[m++]=' ';
+		for(i=0;i<10;i++)
+			str[m++]=num[rand()%10];
+		str[m]=0;
+		printf("%d ",k);
+		fprintf(fd,"%s\n",str);
+	}
+	fclose(fd);
+	return 0;
 }
 
